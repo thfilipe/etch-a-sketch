efe950fbb6beebf7c5008e0bbc8fd12e26e951df
thfilipe <thomasfilipe@yahoo.com>
2022-07-06 08:20:10 +0100

    Create 16 div columns

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,11 @@
 const container = document.getElementById('container');
 
 
-
 for (let i = 0; i < 16; i++) {
-    const divGrid = document.createElement('div');
-    divGrid.classList.add('grid');
-    divGrid.style.cssText = 'border: solid; width: 50px; height: 50px;'
-    container.appendChild(divGrid)
+    for (let i = 0; i < 16; i++) {
+        const divGrid = document.createElement('div');
+        divGrid.classList.add('grid');
+        divGrid.style.cssText = 'border: solid; width: 25px; height: 25px;'
+        container.appendChild(divGrid)
+    }
 }
-
