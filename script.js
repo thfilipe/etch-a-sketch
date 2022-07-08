539992a66e86d152589350e3f2750894f6973b66
thfilipe <thomasfilipe@yahoo.com>
2022-07-08 09:43:42 +0100

    Change grid to function using CSS grid

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const container = document.getElementById('container');
+function createGrid() {
+    const board = document.getElementById('board');
+    board.style.gridTemplateColumns = 'repeat(16, 1fr)'
+    board.style.gridTemplateRows = 'repeat(16, 1fr)'
 
-// create 16 by 16 boxes
-for (let i = 0; i < 16; i++) {
-    for (let i = 0; i < 16; i++) {
-        const box = document.createElement('div');
-        box.classList.add('grid');
-        box.style.cssText = 'border: solid; width: 25px; height: 25px;';
-        container.appendChild(box);
+    for (let i = 0; i < 256; i++) {
+        let box = document.createElement('div');
+        box.style.backgroundColor = "red";
+        board.insertAdjacentElement("beforeend", box);
     }
 }
+createGrid()
+// create 16 by 16 boxes
+//for (let i = 0; i < 16; i++) {
+// for (let i = 0; i < 16; i++) {
+//     const box = document.createElement('div');
+//     box.classList.add('column');
+//     box.style.cssText = 'height: 50px; width: 50px; background-color: blue;';
+//     board.appendChild(box);
+// }
+//}
 
 
 
 
-const grid = document.querySelectorAll('.grid');
 
-grid.forEach(box => {
-    box.addEventListener('mouseover', function onClick(e) {
-        e.target.style.backgroundColor = 'salmon';
-    });
+// const grid = document.querySelectorAll('.grid');
 
-});
+// grid.forEach(box => {
+//     box.addEventListener('mouseover', function onClick(e) {
+//         e.target.style.backgroundColor = 'salmon';
+//     });
+
+// });
 
 
